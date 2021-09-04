cbf8531f9b207c33e8d72fa146606595cbc26206
PatrykFij <patryk.fijalkowski1@users.noreply.github.com>
2021-09-04 17:53:27 +0200

    Added redirect from react router dom

--- src/pages/ConstructionPage.js
+++ src/pages/ConstructionPage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import hairdresser from "assets/hairdresser.png";
+import { Redirect } from "react-router";
 
 export const ConstructionPage = () => {
   return (
     <>
-      <a href="/test">
+      <div onClick={() => <Redirect to="/test" />}>
         <img src={hairdresser} className="App-logo" alt="logo" />
-      </a>
+      </div>
       <h2>Strona w przebudowie</h2>
       <h3>www.salonjulia.pl</h3>
     </>
